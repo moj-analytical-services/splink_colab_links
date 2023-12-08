cf1bf85645d1b103f448d74240715980dc08161b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-12-08 10:03:39 +0000

    pyspark

--- add_pip_install.py
+++ add_pip_install.py
@@ -7,9 +7,9 @@ def add_install_cell_to_notebook(notebook_path, output_path):
     with open(notebook_path, "r", encoding="utf-8") as f:
         notebook = json.load(f)
 
-    new_cell_content = ["!pip install splink"]
+    new_cell_content = ["!pip install splink\n"]
     if "spark" in notebook_path:
-        new_cell_content.append("!pip install spark")
+        new_cell_content.append("!pip install pyspark\n")
 
     new_cell = {
         "cell_type": "code",
